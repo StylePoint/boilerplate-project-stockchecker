2be0d5102340f7c6ca48d7ce1207526bccc34948
up202010351 <joaomicaelo.costa@gmail.com>
2022-03-22 01:06:15 +0000

    Stock checker exam done

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet = require("helmet");
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -16,7 +18,16 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"]
+    }
+  })
+);
 
+app.enable('trust proxy');
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
@@ -37,7 +48,7 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(port , function () {
   console.log('Your app is listening on port ' + listener.address().port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
